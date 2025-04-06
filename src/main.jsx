b5ad9779479dc53af92ab0b0f558989c55a71033
Patrick Oser <patrick.oser148@gmail.com>
2025-04-06 13:07:59 -0400

    Wrap App in StyleSheetManager.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StyleSheetManager } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StyleSheetManager shouldForwardProp={isPropValid}>
+      <App />
+    </StyleSheetManager>
   </StrictMode>,
 )
